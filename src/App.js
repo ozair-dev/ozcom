2c42c0f5eedb843a3c270c7d84fee94776b57e3b
truemail785 <truemail785@gamil.com>
2021-01-27 22:48:00 +0500

    f

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default withRouter(
 		}
 
 		componentDidMount(){
-			axios.get("https://ozcom-backend.herokuapp.com/user/")
+			axios.get("https://ozcom-backend.herokuapp.com/user")
 			.then(res=>{
 				this.updateUser(res.data.user)
 			})
